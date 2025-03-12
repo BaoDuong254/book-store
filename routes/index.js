7edfda5d10fa991ec6f43224c505a82347333a80
GiaBao <duonggiabao254@gmail.com>
2025-03-12 18:23:29 +0700

    first push

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const Book = require("../model/book.model");
+
+// Read books
+router.get('/', async (req, res) => {
+    let books = await Book.find()
+    res.render('index', {books: books})
+});
+
+// Create books
+router.post('/', (req, res) => {
+    let newBook = new Book()
+    newBook.title = req.body.title;
+    newBook.description = req.body.description;
+    newBook.author = req.body.author;
+    newBook.save()
+        .then(function (err) {
+            if (err) {
+                console.log(err)
+            }
+        })
+    res.redirect('/')
+})
+
+// Update books
+router.post("/update", (req, res) => {
+    let id = req.body.id;
+    Book.findById(id)
+        .then(book => {
+            book.title = req.body.title;
+            book.description = req.body.description;
+            book.author = req.body.author;
+            book.save()
+                .then(() => {
+                    res.redirect('/')
+                })
+        })
+        .catch(err => console.log(err));
+})
+
+// Delete books
+router.post("/delete", (req, res) => {
+    let id = req.body.id
+    Book.findByIdAndDelete(id)
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch(err => console.log(err))
+})
+
+module.exports = router;
